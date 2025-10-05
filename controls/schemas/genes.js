b92db7c4519fd9373c65fd84a8eb7e297aad57e1
linewelder <redalerm@gmail.com>
2025-10-05 14:31:44 +0200

    Display gene values in UI

--- controls/schemas/genes.js
+++ controls/schemas/genes.js
@@ -5,5 +5,5 @@ export default Object.entries(GENES)
         name: internalName,
         label: gene.label,
         type: 'label',
-        defaultValue: `${gene.emoji} — ${gene.label}`,
+        defaultValue: `${gene.emoji} — ${gene.label} (${gene.value})`,
     }));
